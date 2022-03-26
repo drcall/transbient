23c4e6d6e7d29b1fd67141aba96c317214bc15c2
drcall <drcall4@outlook.com>
2022-03-26 14:45:17 -0400

    Django environ

--- transbient/settings.py
+++ transbient/settings.py
@@ -13,19 +13,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import sys
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gth#$$gf0l-=9v6le7um+veh3w3d!!3=w^shkwdv^lqg-yjz*s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -86,10 +93,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'transbient-main',
+            'NAME': env('DB_NAME'),
             'USER': 'postgres',
-            'PASSWORD': 'hoohacks2022',
-            'HOST': '34.145.227.188',
+            'PASSWORD': env('PASSWORD'),
+            'HOST': env('HOST'),
             'PORT':  '5432',
             'OPTIONS': {
                 'sslmode': 'verify-ca', #leave this line intact
