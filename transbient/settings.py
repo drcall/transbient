6793032a5eedb65ef0cc48bebfa7e10a06babe2a
drcall <drcall4@outlook.com>
2022-03-26 15:58:53 -0400

    Default site protocol

--- transbient/settings.py
+++ transbient/settings.py
@@ -159,6 +159,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
