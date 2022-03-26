d9cd75c3bd66920a03e775adfc842bacbe93257f
drcall <drcall4@outlook.com>
2022-03-26 17:11:08 -0400

    Allowedhosts issue

--- transbient/settings.py
+++ transbient/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['transbient.herokuapp.com']
 
 
 # Application definition
