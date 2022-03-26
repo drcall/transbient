6045f74b0bc30a4dc8fcd2a91817e9c61d9de8d4
Bejoy Sen <bs7fhr@virginia.edu>
2022-03-26 18:01:32 -0400

    added 127.0.0.1

--- transbient/settings.py
+++ transbient/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['transbient.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['transbient.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
