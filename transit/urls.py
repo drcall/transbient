efba365ce0b4da3e56985f071dca81b682d5387e
drcall <drcall4@outlook.com>
2022-03-27 02:32:29 -0400

    Navbar links, urls, templates

--- transit/urls.py
+++ transit/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="transit/dashboard.html")),
+    path('settings/', TemplateView.as_view(template_name="transit/settings.html")),
     path('accounts/', include('allauth.urls')),
 ]
