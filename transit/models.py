736926317a049b9704d37a60f5e180f8012bd83d
Bejoy Sen <bs7fhr@virginia.edu>
2022-03-27 03:42:10 -0400

    Created route,vehicle,stop models

--- transit/models.py
+++ transit/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Route(models.Model):
+    id = models.IntegerField(primary_key=True)
+    long_name = models.CharField(max_length=35)
+    short_name = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+
+class Stop(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    long = models.IntegerField()
+    lat = models.IntegerField()
+    code = models.IntegerField()
+    routes = models.ManyToManyField(Route)
+
+class Vehicle(models.Model):
+    id = models.IntegerField(primary_key=True)
+    call_name = models.CharField(max_length=20)
+    long = models.IntegerField()
+    lat = models.IntegerField()
+    service_status = models.CharField(max_length=12)
+    route_id = models.ForeignKey(Route,on_delete=models.DO_NOTHING)
