41b7b3cbb13959ed791b5132ead1482f06a87c06
drcall <drcall4@outlook.com>
2022-03-27 03:00:10 -0400

    Add phone number support to model

--- transit/models.py
+++ transit/models.py
@@ -6,14 +6,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class UserSettings(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     threshold_eta = models.IntegerField(default=5)
 
 class UserSettingsForm(forms.ModelForm):
     model = UserSettings
-    fields = ['author', 'phone', 'threshold_eta']
+    fields = ['phone', 'threshold_eta']
     widgets = {
-            'phone': forms.TextInput(attrs={'placeholder': _('Phone')}),
+            'phone': forms.TextInput(attrs={'placeholder': ('Phone')}),
             'start': forms.TextInput(attrs={'type': 'range', 'min': '1', 'max': '10'}),
         }
